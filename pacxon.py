f741d44f41bd379ad293e35a6d262962e2aa9036
kamsza <kamsza@onet.pl>
2019-03-24 22:27:56 +0100

    Poprawiona funkcja obsługująca zderzenia duszków ze ścianami

--- pacxon.py
+++ pacxon.py
@@ -3,14 +3,13 @@ from random import choice
 
 clock = pygame.time.Clock()
 done = False
-start_map_file = open( "tailmap_for_tests.txt", "r")
+start_map_file = open( "tailmap_for_tests_big.txt", "r")
 tile_map = [[int(n) for n in line.split()] for line in start_map_file]
 
 textures = {
     1: pygame.image.load('occ.png'),
     0: pygame.image.load('free.png')
 }
-
 
 tile_size = 25
 map_width = len(tile_map[0])
@@ -21,16 +20,18 @@ screen_height = map_height * tile_size
 
 class Ghost:
     def __init__(self):
-        self.x = screen_width / 2
-        self.y = screen_height / 2
-        self.speed = 0.2
-        self.dx = self.speed * choice([-1,1])
-        self.dy = self.speed * choice([-1,1])
         self.img = pygame.image.load('ghost.png')
-        self.height, self.width = self.img.get_rect().size
+        self.width, self.height = self.img.get_rect().size
+        self.speed = 2.5                                                # 12.5 divided by speed must give natural number
+        self.x = map_width // 2 * tile_size + 0.5 * tile_size
+        self.y = map_height // 2 * tile_size + 0.5 * tile_size
+        self.dx = self.speed * choice([-1, 1])
+        self.dy = self.speed * choice([-1, 1])
 
     def draw(self):
-        screen.blit(self.img, (self.x + 1, self.y))
+        img_x = self.x + (tile_size - self.width) / 2
+        img_y = self.y + (tile_size - self.height) / 2
+        screen.blit(self.img, (img_x, img_y))
 
     def move(self):
         self.x += self.dx
@@ -38,26 +39,47 @@ class Ghost:
         self.check_collision()
 
     def check_collision(self):
-        center_x = (self.x + self.width/2)
-        center_y = (self.y + self.height / 2)
+        center_x = self.x + tile_size / 2
+        center_y = self.y + tile_size / 2
+        dist_to_center_x = abs(center_x % tile_size - tile_size / 2)
+        dist_to_center_y = abs(center_y % tile_size - tile_size / 2)
         x_ind = int(center_x / tile_size)
         y_ind = int(center_y / tile_size)
+        collides = False
 
-        if abs(center_x % tile_size - tile_size / 2) <= self.speed:
-            if tile_map[y_ind][x_ind + 1]:
-                if self.dx > 0: self.dx = -self.dx
-            elif tile_map[y_ind][x_ind - 1]:
-                if self.dx < 0: self.dx = -self.dx
-        elif abs(center_y % tile_size - tile_size / 2) <= self.speed:
-            if tile_map[y_ind - 1][x_ind]:
-                if self.dy < 0: self.dy = -self.dy
-            elif tile_map[y_ind + 1][x_ind]:
-                if self.dy > 0: self.dy = -self.dy
+        if dist_to_center_x < self.speed and dist_to_center_y < self.speed:
+            collides = True
+            if tile_map[y_ind - 1][x_ind] and self.dy < 0:
+                    self.dy = -self.dy
+            elif tile_map[y_ind + 1][x_ind] and self.dy > 0:
+                    self.dy = -self.dy
+            elif tile_map[y_ind][x_ind - 1] and self.dx < 0:
+                    self.dx = -self.dx
+            elif tile_map[y_ind][x_ind + 1] and self.dx > 0:
+                    self.dx = -self.dx
+            elif tile_map[y_ind - 1][x_ind + 1] and self.dx > 0 and self.dy < 0:
+                    self.dx = -self.dx
+                    self.dy = -self.dy
+            elif tile_map[y_ind + 1][x_ind + 1] and self.dx > 0 and self.dy > 0:
+                    self.dx = -self.dx
+                    self.dy = -self.dy
+            elif tile_map[y_ind + 1][x_ind - 1] and self.dx < 0 and self.dy > 0:
+                    self.dx = -self.dx
+                    self.dy = -self.dy
+            elif tile_map[y_ind - 1][x_ind - 1] and self.dx < 0 and self.dy < 0:
+                    self.dx = -self.dx
+                    self.dy = -self.dy
+            else:
+                collides = False
+        if collides:
+            self.check_collision()
 
 
 pygame.init()
 screen = pygame.display.set_mode((screen_width, screen_height))
-ghosts = Ghost()
+ghosts = []
+for i in range(0, 9):
+    ghosts.append(Ghost())
 
 while not done:
     for event in pygame.event.get():
@@ -68,8 +90,9 @@ while not done:
         for column in range(map_width):
             screen.blit(textures[tile_map[row][column]], (column * tile_size, row * tile_size))
 
-    ghosts.draw()
-    ghosts.move()
+    for ghost in ghosts:
+        ghost.draw()
+        ghost.move()
 
     pygame.display.flip()
     pygame.display.update()
